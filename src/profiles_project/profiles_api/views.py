0e2adc3357fddfa3d6e78310966db28c457760eb
Bernardo Laing <bernardo.laing@gmail.com>
2018-01-29 08:07:29 -0600

    Created Hello World APIView

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HelloAPIView(APIView):
+    def get(self, request, format=None):
+        an_apiview = [
+            'Uses HTTP method as function (get, put, post, patch, delete)',
+            'It is similar to a django view',
+            'Gives you most control overyour logic',
+            'mapped manually to URLs'
+        ]
+
+        return Response({'message': 'Hello World!', 'an_apiview': an_apiview})
+        
